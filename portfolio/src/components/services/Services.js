bffc1c77bbd7857125565876dc95fff296cd6d42
rita <ritaayalew6@gmail.com>
2025-02-24 02:42:53 +0300

    modify services page of my portfolio

--- portfolio/src/components/services/Services.js
+++ portfolio/src/components/services/Services.js
@@ -7,6 +7,7 @@ import certeficate3 from "../../images/certeficate-3.png";
 import certeficate4 from "../../images/certeficate-4.png";
 import certeficate5 from "../../images/certeficate-5.png";
 import certeficate6 from "../../images/certeficate-6.png";
+import Button from '../get-in-touch-btn/Button';
 
 const Services = () => {
   return (
@@ -19,7 +20,7 @@ const Services = () => {
           <p className={styles.detail}>Creating visually appealing and interactive user interfaces using HTML, CSS, and JavaScript.</p>
           <h3>Back-End Development</h3>
           <p>Building robust server-side applications and APIs using technologies like Node.js, Express, and databases.</p>
-          <button className={styles['call-to-action-btn']} onClick={() => window.location.href='contact.html'}>Get in Touch</button>
+          <Button/>
         </section>
 
         <section className={`${styles.partitions} ${styles['skill-set']}`}>
@@ -27,35 +28,35 @@ const Services = () => {
           <div>
             <ul className={styles['skill-icons']}>
               <li>
-                <i className={`${styles.icon} fa-brands fa-html5`}></i>
+                <i className={`${styles.icon} ${styles.fahtml} fa-brands fa-html5`}></i>
                 <h4>HTML5</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-brands fa-css3-alt`}></i>
+                <i className={`${styles.icon} ${styles.facss}  fa-brands fa-css3-alt`}></i>
                 <h4>CSS3</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-brands fa-js`}></i>
+                <i className={`${styles.icon} ${styles.fajs}  fa-brands fa-js`}></i>
                 <h4>JavaScript</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-brands fa-node-js`}></i>
+                <i className={`${styles.icon} ${styles.fanode}  fa-brands fa-node-js`}></i>
                 <h4>Node.js</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-brands fa-react`}></i>
+                <i className={`${styles.icon} ${styles.fareact}  fa-brands fa-react`}></i>
                 <h4>React</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-solid fa-database`}></i>
+                <i className={`${styles.icon} ${styles.fadatabase}  fa-solid fa-database`}></i>
                 <h4>MySQL</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-brands fa-git-alt`}></i>
+                <i className={`${styles.icon} ${styles.fagit}  fa-brands fa-git-alt`}></i>
                 <h4>Git</h4>
               </li>
               <li>
-                <i className={`${styles.icon} fa-solid fa-database`}></i>
+                <i className={`${styles.icon} ${styles.fadatabase}  fa-solid fa-database`}></i>
                 <h4>MongoDB</h4>
               </li>
             </ul>
@@ -67,7 +68,7 @@ const Services = () => {
         <h2>Certeficates</h2>
         <p>Qualifications over the years.</p>
         <div className={styles.scrollable}>
-          <button className={styles['left-btn']}><i className="fa-solid fa-angle-left"></i></button>
+          <button className={styles['left-btn']}><i className={`fa-solid fa-angle-left ${styles.fasolid}`}></i></button>
           <div className={styles['proof-images']}> 
             <img src={certeficate1} alt="certeficate-1"/>
             <img src={certeficate2} alt="certeficate-2"/>
@@ -76,7 +77,8 @@ const Services = () => {
             <img src={certeficate5} alt="certeficate-5"/>
             <img src={certeficate6} alt="certeficate-6"/>
           </div>
-          <button className={styles['right-btn']}><i className="fa-solid fa-angle-right"></i></button>
+          
+          <button className={styles['right-btn']}><i className={`fa-solid fa-angle-right ${styles.fasolid}`}></i></button>
         </div>
       </section>
     </div>
