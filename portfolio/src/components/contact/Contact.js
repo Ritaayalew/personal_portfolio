af47ddd6ecf08a089a79dc6554737ea9342a9437
rita <ritaayalew6@gmail.com>
2025-03-01 22:34:32 +0300

    add contact form that accepts email in the contact page

--- portfolio/src/components/contact/Contact.js
+++ portfolio/src/components/contact/Contact.js
@@ -1,32 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './contact.module.css';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_9fk63yq', 'template_evvko1k', form.current, {
+        publicKey: 'UWWvsu9T9Z-pUrkjv',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
+
   return (
     <div className={styles.black}>
     <div className={styles.main}>
       <section className={styles['small-box']}>
         <h2>Reach out to me</h2>
         <p>I'm open to hearing about your ideas and what you are seeking for your website. Send me an email, call me, or write to me on my social networks and I'll make sure I get back to you.</p>
-      </section>
-
-      <section className={styles.contact}>
-        <h1>Contact</h1>
-        <div></div>
-        <div className={styles['contact-method']}>
-          <i className="fa-solid fa-phone"></i>
-          <p><b>Phone number</b>: +251-909889844</p>
-        </div>
-        <div className={styles['contact-method']}>
-          <i className="fa-solid fa-envelope"></i>
-          <p><b>Email</b>: ritaayalew6@gmail.com</p>
-        </div>
-        <div className={styles['contact-method']}>
-          <i className="fa-solid fa-location-dot"></i>
-          <p><b>Address</b>: Arat killo, Addis Ababa, Ethiopia</p>
-        </div>
-        <p className={styles.descrption}>You can also find me on the platforms below.</p>
-
         <div className={styles.icons}>
           <a href="https://www.linkedin.com/in/rita-ayalew-354802308/" aria-label="Linkedin">
             <i className={`fab fa-linkedin ${styles.falinkedin}`} aria-hidden="true"></i>
@@ -41,6 +44,46 @@ const Contact = () => {
             <i className={`fab fa-telegram ${styles.fatelegram}`} aria-hidden="true"></i>
           </a>
         </div>
+      </section>
+
+      <section className={styles.contact}>
+        <h1>Contact</h1>
+
+        <div className={styles['contact-method']}>
+          <i className="fa-solid fa-phone"></i>
+          <p><b>Phone number</b>: +251-909889844</p>
+        </div>
+        <div className={styles['contact-method']}>
+          <i className="fa-solid fa-envelope"></i>
+          <p><b>Email</b>: ritaayalew6@gmail.com</p>
+        </div>
+        <div className={styles['contact-method']}>
+          <i className="fa-solid fa-location-dot"></i>
+          <p><b>Address</b>: Arat killo, Addis Ababa</p>
+        </div>
+
+        <form className={styles.form} ref={form} onSubmit={sendEmail}>
+          <div className={styles.each}>
+            <label>Name: </label>
+            <input type="text" name="user_name" />
+          </div>
+
+          <div className={styles.each}>
+            <label>Email: </label>
+            <input type="email" name="user_email" />
+          </div>
+
+          <div className={styles.each}>
+            <label>Message: </label>
+            <textarea name="message" />
+          </div>
+          
+          <input className={styles.btn} type="submit" value="Send" />
+        </form>
+    
+
+
+        
       </section>
     </div>
     </div>
